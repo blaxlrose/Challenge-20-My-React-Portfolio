989f425ca71195e4fea14729770df8eda9f06c95
christopher brown <christopherbrown@christohersMini.fios-router.home>
2023-08-07 12:09:22 -0400

    footer and images

---
+++ my/src/components/Footer.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+function Footer() {
+	return (
+		<footer className="footer">
+			<div>
+				<a
+					href="https://github.com/blaxlrose"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<img
+						src={require("./images/github-logo.png")}
+						alt="Github"
+						className="logo"
+					></img>
+				</a>
+			</div>
+			<div>
+				<a
+					href="https://www.linkedin.com/in/christopher-brown-6b6553261/"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<img
+						src={require("./images/linkedin-logo.png")}
+						alt="LinkedIn"
+						className="logo"
+					></img>
+				</a>
+			</div>
+			<div>
+				<a
+					href="https://twitter.com/mister_garak"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<img
+						src={require("./images/threads-logo.png")}
+						alt="Twitter"
+						className="logo"
+					></img>
+				</a>
+			</div>
+		</footer>
+	);
+}
+
+export default Footer;
