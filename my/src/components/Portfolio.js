aa023f074d45abdc81ec4e6e6097951ab753ee87
christopher brown <christopherbrown@christohersMini.fios-router.home>
2023-08-07 17:37:08 -0400

    project and portfolio

--- my/src/components/Portfolio.js
+++ my/src/components/Portfolio.js
@@ -1,115 +1,42 @@
 import React from 'react';
-import Project from './Project';
+import projects from './Project'; // Assuming the projects.js file is in the same directory
 
-function Portfolio() {
-    const projects = [
-        {
-            name: 'DailyCalm',
-            description:
-                'This app helps you search for ways to bring self care into your life. It provides daily suggestions that you can use. It also keeps score of what self care activities you have completed',
-            image: '',
-            technologies: [
-                'HTML/CSS',
-                'JavaScript',
-                'Node.js',
-                'GraphQL',
-                'Heroku',
-                'React.js',
-                'MondoD',
-                'JWT Authentication',
-                'Bulma CSS',
-                'React CSS',
-                'MERN Stack',
-            ],
-            github: 'https://github.com/ckc2007/DailyCalm',
-            deployed: 'https://daily-calm-2023-be74e09f75dd.herokuapp.com/',
-        },
-        {
-            name: 'What The Hells For Dinner',
-            description:
-                'This is an app that helps you create meals using whatever you find in your kitchen. You choose your ingredients and then the app gives you possible recipes.' ,
-            image: '',
-            technologies: [
-                'HTML/CSS',
-                'JavaScript',
-                'APIs',
-                'Bulma',
-                'Heroku',
-            ],
-            github: 'https://github.com/FiggersRM/WTHFD',
-            deployed: 'https://figgersrm.github.io/WTHFD/',
-        },
-        {
-            name: 'Timekeeper',
-            description:
-                'This is a timekeeping app that allows you to plan daily, weekly and monthly',
-            image: '',
-            technologies: ['HTML/CSS', 'JavaScript', 'Bulma', 'Sequelize/MySQL'],
-            github: 'https://github.com/benbryant98/timekeeper',
-            deployed: 'https://nameless-ravine-04537.herokuapp.com/',
-        },
-        
-        {
-            name: 'Weather Dashboard',
-            description:
-                'A weather dashboard that shows current weather and five-day weather forecast for any city.',
-            image: '',
-            technologies: ['HTML/CSS', 'JavaScript', 'Api'],
-            github: 'https://github.com/blaxlrose/challenge-6-weather-dashboard',
-            deployed: 'https://blaxlrose.github.io/challenge-6-weather-dashboard/',
-        },
-        {
-            name: 'PWA Text Editor',
-            description:
-                'The application is a web text editor where the user can create notes or code snippets with or without an internet connection and where the user can reliably retrieve them for later use.',
-            image: '',
-            technologies: ['HTML/CSS', 'JavaScript'],
-            Github: 'https://github.com/blaxlrose/challenge-19-PWA-Text-Editor',
-            deployed: 'https://sleepy-retreat-08707-7164d0d1ae9b.herokuapp.com',
-        },
-        {
-            name: 'ReadMe Generator',
-            description:
-                'This is a simple app using node.js to create a README file for your repository.',
-            image: '',
-            technologies: ['JavaScript', 'Node.js'],
-            github: 'https://github.com/blaxlrose/Challenge-9-Professional-README-Generator',
-        },
-    ];
-
-    return (
-        <section>
-            <div className="center">
-                <h1 className="page-header">My Portfolio</h1>
-            </div>
-            <div>
-                <ul className="flex-row mobile-row">
-                    <li className="padding">
-                        <Project projects={projects[0]}></Project>
-                    </li>
-                    <li className="padding">
-                        <Project projects={projects[1]}></Project>
-                    </li>
-                </ul>
-                <ul className="flex-row mobile-row">
-                    <li className="padding">
-                        <Project projects={projects[2]}></Project>
-                    </li>
-                    <li className="padding">
-                        <Project projects={projects[3]}></Project>
-                    </li>
-                </ul>
-                <ul className="flex-row mobile-row">
-                    <li className="padding">
-                        <Project projects={projects[4]}></Project>
-                    </li>
-                    <li className="padding">
-                        <Project projects={projects[5]}></Project>
-                    </li>
-                </ul>
-            </div>
-        </section>
-    );
-}
+const Portfolio = () => {
+  return (
+    <div className="container">
+      <h1 className="text-center mb-5">My Portfolio</h1>
+      {Array.from({ length: 3 }, (_, rowIndex) => (
+        <div className="row mb-4" key={rowIndex}>
+          {Array.from({ length: 2 }, (_, colIndex) => {
+            const projectIndex = rowIndex * 2 + colIndex;
+            const project = projects[projectIndex];
+            return project ? (
+              <div key={colIndex} className="col-md-6 mb-4">
+                <div className="card custom-card">
+                  <img src={project.image} className="card-img-top" alt={project.name} />
+                  <div className="card-body">
+                    <h5 className="card-title">{project.name}</h5>
+                    <p className="card-text">{project.description}</p>
+                    <p className="card-text">
+                      <strong>Technologies:</strong> {project.technologies.join(', ')}
+                    </p>
+                    <a href={project.github} target="_blank" rel="noopener noreferrer" className="btn btn-primary">
+                      GitHub
+                    </a>
+                    <a href={project.deployed} target="_blank" rel="noopener noreferrer" className="btn btn-primary">
+                      Deployed
+                    </a>
+                  </div>
+                </div>
+              </div>
+            ) : (
+              <div className="col-md-6 mb-4"></div>
+            );
+          })}
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default Portfolio;
--- my/src/components/Project.js
+++ my/src/components/Project.js
@@ -1,56 +1,87 @@
-import React, { useState } from "react";
-import Card from "react-bootstrap/Card";
+import Daily from '../images/dailycalm1.png';
+import What from '../images/WTHFD.png';
+import Time from '../images/timekeeper.png';
+import PWA from '../images/pwa.png';
+import Weather from '../images/weather.png';
+import Readme from '../images/readme.png';
 
-function Project(props) {
-	// This imports the information for the current project sent down.
-	const currentProject = useState(props)[0].projects;
+    const Project = [
+        {
+            name: 'DailyCalm',
+            description:
+                'This app helps you search for ways to bring self care into your life. It provides daily suggestions that you can use. It also keeps score of what self care activities you have completed',
+            image: Daily,
+            technologies: [
+                'HTML/CSS',
+                'JavaScript',
+                'Node.js',
+                'GraphQL',
+                'Heroku',
+                'React.js',
+                'MondoD',
+                'JWT Authentication',
+                'Bulma CSS',
+                'React CSS',
+                'MERN Stack',
+            ],
+            github: 'https://github.com/ckc2007/DailyCalm',
+            deployed: 'https://daily-calm-2023-be74e09f75dd.herokuapp.com/',
+        },
+        {
+            name: 'What The Hells For Dinner',
+            description:
+                'This is an app that helps you create meals using whatever you find in your kitchen. You choose your ingredients and then the app gives you possible recipes.' ,
+            image: What,
+            technologies: [
+                'HTML/CSS',
+                'JavaScript',
+                'APIs',
+                'Bulma',
+                'Heroku',
+            ],
+            github: 'https://github.com/FiggersRM/WTHFD',
+            deployed: 'https://figgersrm.github.io/WTHFD/',
+        },
+        {
+            name: 'Timekeeper',
+            description:
+                'This is a timekeeping app that allows you to plan daily, weekly and monthly',
+            image: Time,
+            technologies: ['HTML/CSS', 'JavaScript', 'Bulma', 'Sequelize/MySQL'],
+            github: 'https://github.com/benbryant98/timekeeper',
+            deployed: 'https://nameless-ravine-04537.herokuapp.com/',
+        },
+        
+        {
+            name: 'Weather Dashboard',
+            description:
+                'A weather dashboard that shows current weather and five-day weather forecast for any city.',
+            image: Weather,
+            technologies: ['HTML/CSS', 'JavaScript', 'Api'],
+            github: 'https://github.com/blaxlrose/challenge-6-weather-dashboard',
+            deployed: 'https://blaxlrose.github.io/challenge-6-weather-dashboard/',
+        },
+        {
+            name: 'PWA Text Editor',
+            description:
+                'The application is a web text editor where the user can create notes or code snippets with or without an internet connection and where the user can reliably retrieve them for later use.',
+            image: PWA,
+            technologies: ['HTML/CSS', 'JavaScript'],
+            Github: 'https://github.com/blaxlrose/challenge-19-PWA-Text-Editor',
+            deployed: 'https://sleepy-retreat-08707-7164d0d1ae9b.herokuapp.com',
+        },
+        {
+            name: 'ReadMe Generator',
+            description:
+                'This is a simple app using node.js to create a README file for your repository.',
+            image: Readme,
+            technologies: ['JavaScript', 'Node.js'],
+            github: 'https://github.com/blaxlrose/Challenge-9-Professional-README-Generator',
+        },
+    ];
 
-	const name = currentProject.name;
-	const description = currentProject.description;
-	const image = currentProject.image;
-	const techs = currentProject.technologies;
-	const appLink = currentProject.deployed;
-	const gitLink = currentProject.github;
+                       
+  
 
-	// This function parses the information from the array and creates a list.
-	function getTechs(techArray) {
-		let techList = "";
-
-		for (var i = 0; i < techArray.length; i++) {
-			if (i === techArray.length - 1) {
-				techList += techArray[i];
-			} else {
-				techList += techArray[i] + ", ";
-			}
-		}
-
-		return techList;
-	}
-
-	return (
-		<Card style={{ width: "18rem" }}>
-			<Card.Img
-				variant="top"
-				src={require(`../images/${image}`)}
-				className="card-image"
-			/>
-			<div className="center">
-				<Card.Body>
-					<Card.Title className="card-title">{name}</Card.Title>
-					<Card.Text className="card-text">{description}</Card.Text>
-					<Card.Subtitle className="card-subtitle">Techs Used</Card.Subtitle>
-					<Card.Text className="card-techs">{getTechs(techs)}</Card.Text>
-					<Card.Link href={appLink} target="_blank" className="card-link">
-						{name} App
-					</Card.Link>
-					<br></br>
-					<Card.Link href={gitLink} target="_blank" className="card-link">
-						{name} Github
-					</Card.Link>
-				</Card.Body>
-			</div>
-		</Card>
-	);
-}
 
 export default Project;
